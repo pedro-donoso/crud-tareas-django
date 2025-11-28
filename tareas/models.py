b194d01ac04656c9c9b31d2b47fa395a78778031
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-28 01:07:58 -0300

    creo models

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
